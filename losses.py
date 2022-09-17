9e45cf2ce4bc50e446bbf9da4e494ec7158e9af7
Rasheedelbouri <rasheed.el-bouri@eng.ox.ac.uk>
2022-09-17 01:21:09 +0100

    removing pdb

--- losses.py
+++ losses.py
@@ -8,7 +8,6 @@ def p_losses(model, diffuser, x_start, t):
     noise = torch.randn_like(x_start)
     x_noisy = diffuser.diffuse(x=x_start, t=t)
     predicted_noise = model.forward(x_noisy, t)
-    import pdb; pdb.set_trace()
     loss = LOSS(noise, predicted_noise)
 
     return loss
